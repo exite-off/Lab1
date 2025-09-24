7c6e67a57f2da6f8ab71585dd7eb71d83f13352c
LoFiSwanL <daiki.ren710@gmai.com>
2025-09-24 20:00:10 +0300

    pull it to my file directory

--- transformSystem.cpp
+++ transformSystem.cpp
@@ -4,7 +4,6 @@
 
 #include "point.h"
 #include <random>
-#include <utility>
 
 using namespace std;
 
@@ -12,12 +11,13 @@ Transform::Transform(vector<AffineTransform> affineTransforms) {
     random_device rd;
     this->gen = mt19937(rd());
     this->dist = uniform_int_distribution<int>(0, affineTransforms.size() - 1);
-    this->transforms = std::move(affineTransforms);
+    this->transforms = affineTransforms;
 }
 
 Point2D Transform::operator()(Point2D point) {
-    // TODO: Implement the transformation logic here
-    return point;
+    const AffineTransform transform = transforms[dist(gen)];
+    const Point2D res = transform.applyToPoint(point);
+    return res;
 }
 
 
