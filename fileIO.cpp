63461aa55887f6950dc4523947b68adf4240b2d8
LoFiSwanL <daiki.ren710@gmai.com>
2025-09-24 17:52:14 +0300

    test esample for IO

---
+++ fileIO.cpp
@@ -0,0 +1,40 @@
+#include "fileIO.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void FileIO::read_file(const string &filepath, int &N, Point2D &startPoint, vector<AffineTransform> &transforms)
+{
+    ifstream file(filepath);
+    if(!file){
+        cerr << "cant open file" << endl;
+        return;
+    }
+    file >> N;
+    file >> startPoint.x >> startPoint.y;
+
+    double a11, a12, a21, a22, b1, b2;
+    while(file >> a11 >> a12 >> a21>> a22>> b1>> b2){
+        AffineTransform t(a11, a12, a21, a22, b1, b2);
+        transforms.push_back(t);
+    }
+    
+    if(transforms.empty()){
+        cerr << "file cannot exist affine transform" << endl;
+        return;
+    }
+}
+
+void FileIO::write_in_file(const string &filepath, const vector<Point2D> &points)
+{
+    ofstream output_file(filepath);
+    if(!output_file){
+        cerr << "cant open file for output" << endl;
+        return;
+    }
+
+    for(const auto &p: points){
+        output_file << p.x << " " << p.y << endl;
+    }
+}
---
+++ fileIO.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <random>
+#include "point.h"
+#include "affineTransform.h"
+
+class FileIO{
+public: 
+    static void read_file(const std::string &filepath, int &N, Point2D &startPoint, std::vector<AffineTransform> &transforms);
+
+    static void write_in_file(const std::string &filepath, const std::vector<Point2D> &points);
+};
